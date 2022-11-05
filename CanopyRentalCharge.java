2cc13a7e31a7bac4cb8e83f9adf50b7379157819
KesunyianAyam <38789073+KesunyianAyam@users.noreply.github.com>
2022-11-06 00:34:56 +0800

    formatting

--- CanopyRentalCharge.java
+++ CanopyRentalCharge.java
@@ -1,18 +1,18 @@
 import java.util.*;
 
-public class CanopyRentalCharge{
+public class CanopyRentalCharge {
 
-   public static void main(String [] args){
-      
+   public static void main(String[] args) {
+
       Scanner input = new Scanner(System.in);
       CanopyRentalCharge obj = new CanopyRentalCharge();
-      
+
       String participantType;
       char participantCode, itemCode, prompt;
       int totalCanopies, totalDays;
       double addCharge, canopyCharge, grandTotal;
-      
-      do{
+
+      do {
          System.out.println("UiTM P. Pinang");
          System.out.println("Entrepreneurship Week");
          System.out.println("Canopy Rent System by Ismail Arif (github.io/mayelz)");
@@ -29,48 +29,47 @@ public class CanopyRentalCharge{
          System.out.println("W = Sell wet items, N = Otherwise");
          System.out.print("Item Code: ");
          itemCode = input.next().charAt(0);
-         if(participantCode == 'T')
+         if (participantCode == 'T')
             participantType = "Students";
-         else if(participantCode == 'S')
+         else if (participantCode == 'S')
             participantType = "Staffs";
          else
             participantType = "Outsider";
-         
+
          canopyCharge = obj.canopyCharges(participantCode, totalCanopies, totalDays);
          addCharge = obj.addCharges(itemCode);
          grandTotal = canopyCharge + addCharge;
-         
+
          System.out.println();
          System.out.println("UiTM P. Pinang");
          System.out.println("ORDER RECEIPT");
          System.out.println();
-         System.out.println("Participant type                  : "+participantType);
-         System.out.println("Total canopies                    : "+totalCanopies);
-         System.out.println("Total Days of Rental              : "+totalDays);
+         System.out.println("Participant type                  : " + participantType);
+         System.out.println("Total canopies                    : " + totalCanopies);
+         System.out.println("Total Days of Rental              : " + totalDays);
          System.out.println();
-         System.out.println("TOTAL CHARGES (CANOPY ONLY)       : "+canopyCharge);
-         System.out.println("TOTAL ADDITIONAL CHARGES(IF ANY)  : "+addCharge);
-         System.out.println("GRAND TOTAL                       : "+grandTotal);
+         System.out.println("TOTAL CHARGES (CANOPY ONLY)       : " + canopyCharge);
+         System.out.println("TOTAL ADDITIONAL CHARGES(IF ANY)  : " + addCharge);
+         System.out.println("GRAND TOTAL                       : " + grandTotal);
          System.out.println();
          System.out.print("Do you want to continue? (Y/N)    : ");
          prompt = input.next().charAt(0);
-         }
-         while(prompt == 'Y');
+      } while (prompt == 'Y');
 
-         input.close();
+      input.close();
    }
-      
-   double canopyCharges(char code, int canopy, int day){
-      if(code == 'T')
-         return canopy*day*90;
-      else if(code == 'S')
-         return canopy*day*150;
+
+   double canopyCharges(char code, int canopy, int day) {
+      if (code == 'T')
+         return canopy * day * 90;
+      else if (code == 'S')
+         return canopy * day * 150;
       else
-         return canopy*day*200;
+         return canopy * day * 200;
    }
-      
-  double addCharges(char itm){
-      if(itm == 'W')
+
+   double addCharges(char itm) {
+      if (itm == 'W')
          return 100;
       else
          return 0;
